61b5151f4c1d0d123cf3f0a2c7cd60272cc6dc85
Irwing <86335744+Irwingduran@users.noreply.github.com>
2023-08-22 18:56:12 -0600

    Add files via upload

---
+++ 5-2-coding.js
@@ -0,0 +1,9 @@
+// two gymnastics teams, 'Dolphins' and 'Koalas'.
+// there is a new gymnastics discipline, which works differently.
+// each team competes 3 times, and then the average os the 3 scores is calculated
+// (so one average score per team)
+// a team only wins if it has at least double the average score od the other team.
+// otherwise, no team wins!
+
+
+
